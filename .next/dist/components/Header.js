5bfb572a24ae4c76718529949b7f306d1fbb5b1d
Edson Machado <edson.machado@grupocmc.es>
2018-10-01 17:40:37 +0200

    Adding a header menu

---
+++ .next/dist/components/Header.js
@@ -0,0 +1,45 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _semanticUiReact = require('semantic-ui-react');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\edson.machado\\Documents\\ethereum_solidity\\kickstart\\components\\Header.js';
+
+exports.default = function () {
+
+  return _react2.default.createElement(_semanticUiReact.Menu, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    }
+  }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, 'CrowdCoin'), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: 'right', __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    }
+  }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    }
+  }, 'Campaigns'), _react2.default.createElement(_semanticUiReact.Menu.Item, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    }
+  }, '+')));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXEhlYWRlci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIk1lbnUiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTLEFBRVQ7Ozs7OztrQkFBZSxZQUFNLEFBRW5COzt5QkFFRSxBQUFDOztnQkFBRDtrQkFBQSxBQUVFO0FBRkY7QUFBQSxHQUFBLGtCQUVHLGNBQUQsc0JBQUEsQUFBTTs7Z0JBQU47a0JBQUE7QUFBQTtBQUFBLEtBRkYsQUFFRSxBQUlBLDhCQUFDLGNBQUQsc0JBQUEsQUFBTSxRQUFLLFVBQVgsQUFBb0I7Z0JBQXBCO2tCQUFBLEFBRUU7QUFGRjtxQkFFRyxjQUFELHNCQUFBLEFBQU07O2dCQUFOO2tCQUFBO0FBQUE7QUFBQSxLQUZGLEFBRUUsQUFJQSw4QkFBQyxjQUFELHNCQUFBLEFBQU07O2dCQUFOO2tCQUFBO0FBQUE7QUFBQSxLQWROLEFBRUUsQUFNRSxBQU1FLEFBVVA7QUExQkQiLCJmaWxlIjoiSGVhZGVyLmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL2Vkc29uLm1hY2hhZG8vRG9jdW1lbnRzL2V0aGVyZXVtX3NvbGlkaXR5L2tpY2tzdGFydCJ9
