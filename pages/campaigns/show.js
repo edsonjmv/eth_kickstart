074d7f15c9ab137104b51d355a43ac3dbc7ecc9b
Edson Machado <edson.machado@grupocmc.es>
2018-10-23 16:17:25 +0200

    Creating a request

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { Card, Grid } from 'semantic-ui-react';
-import Layout from '../../components/Layout';
-import ContributeForm from '../../components/ContributeForm';
-import Campaign from '../../ethereum/campaign';
-import web3 from '../../ethereum/web3';
+import React, { Component } from "react";
+import { Card, Grid, Button } from "semantic-ui-react";
+import Layout from "../../components/Layout";
+import ContributeForm from "../../components/ContributeForm";
+import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+import { Link } from "../../routes";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -33,29 +34,34 @@ class CampaignShow extends Component {
     const items = [
       {
         header: manager,
-        meta: 'Address of Manager',
-        description: 'The manager created this campaign and can create requests to withdraw money',
-        style: { overflowWrap: 'break-word' }
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" }
       },
       {
         header: minimumContribution,
-        meta: 'Minimum Contribution (wei)',
-        description: 'You must contribute at least this much wei to become an approver'
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver"
       },
       {
         header: requestsCount,
-        meta: 'Number of requests',
-        description: 'A request tries to withdraw money from the contract. Request mus be approved by approvers.'
+        meta: "Number of requests",
+        description:
+          "A request tries to withdraw money from the contract. Request mus be approved by approvers."
       },
       {
         header: approversCount,
-        meta: 'Number of Approvers',
-        description: 'Number of people who have already donated to this campaign'
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign"
       },
       {
-        header: web3.utils.fromWei(balance, 'ether'),
-        meta: 'Campaign Balance (ether)',
-        description: 'The balance is how much money this campaign has left to spend'
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend"
       }
     ];
 
@@ -67,12 +73,21 @@ class CampaignShow extends Component {
       <Layout>
         <h3>Campaign show</h3>
         <Grid>
-          <Grid.Column width={10}>
-            {this.renderCards()}
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <ContributeForm address={this.props.address} />
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+            <Grid.Column width={6}>
+              <ContributeForm address={this.props.address} />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Link route={`/campaigns/${this.props.address}/requests`}>
+                <a>
+                  <Button primary>View Requests</Button>
+                </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </Layout>
     );
